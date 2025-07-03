890e5fd225f0e8e4291a0cc68af7aaece0a61f93
KUCA Brice <kuca.b@sfeir.com>
2025-07-03 16:05:23 +0200

    fix

--- front/src/gql/graphql.ts
+++ front/src/gql/graphql.ts
@@ -1,166 +1,167 @@
 /* eslint-disable */
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
-export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+export type MakeEmpty<
+  T extends { [key: string]: unknown },
+  K extends keyof T,
+> = { [_ in K]?: never };
+export type Incremental<T> =
+  | T
+  | {
+      [P in keyof T]?: P extends " $fragmentName" | "__typename" ? T[P] : never;
+    };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: { input: string; output: string; }
-  String: { input: string; output: string; }
-  Boolean: { input: boolean; output: boolean; }
-  Int: { input: number; output: number; }
-  Float: { input: number; output: number; }
+  ID: { input: string; output: string };
+  String: { input: string; output: string };
+  Boolean: { input: boolean; output: boolean };
+  Int: { input: number; output: number };
+  Float: { input: number; output: number };
   /** `Date` type as integer. Type represents date and time as number of milliseconds from start of UNIX epoch. */
-  Timestamp: { input: any; output: any; }
+  Timestamp: { input: any; output: any };
   /** The `Upload` scalar type represents a file upload. */
-  Upload: { input: any; output: any; }
+  Upload: { input: any; output: any };
 };
 
 export type AuthResponse = {
-  __typename?: 'AuthResponse';
-  access_token: Scalars['String']['output'];
+  __typename?: "AuthResponse";
+  access_token: Scalars["String"]["output"];
   user: UserDto;
 };
 
 export type CreateUserInput = {
-  email: Scalars['String']['input'];
-  name: InputMaybe<Scalars['String']['input']>;
-  password: Scalars['String']['input'];
+  email: Scalars["String"]["input"];
+  name: InputMaybe<Scalars["String"]["input"]>;
+  password: Scalars["String"]["input"];
 };
 
 export type DeleteFileInput = {
-  fileName: Scalars['String']['input'];
-  id: Scalars['Float']['input'];
+  fileName: Scalars["String"]["input"];
+  id: Scalars["Float"]["input"];
 };
 
 export type Document = {
-  __typename?: 'Document';
-  id: Scalars['Int']['output'];
-  name: Scalars['String']['output'];
-  status: Scalars['String']['output'];
-  types: Scalars['String']['output'];
-  url: Maybe<Scalars['String']['output']>;
+  __typename?: "Document";
+  id: Scalars["Int"]["output"];
+  name: Scalars["String"]["output"];
+  status: Scalars["String"]["output"];
+  types: Scalars["String"]["output"];
+  url: Maybe<Scalars["String"]["output"]>;
   user: User;
-  userId: Scalars['Float']['output'];
+  userId: Scalars["Float"]["output"];
 };
 
 export type GetLogsInput = {
-  types: InputMaybe<Array<Scalars['String']['input']>>;
+  types: InputMaybe<Array<Scalars["String"]["input"]>>;
 };
 
 export type Log = {
-  __typename?: 'Log';
-  id: Scalars['String']['output'];
-  message: Scalars['String']['output'];
-  timestamp: Scalars['Timestamp']['output'];
-  type: Scalars['String']['output'];
+  __typename?: "Log";
+  id: Scalars["String"]["output"];
+  message: Scalars["String"]["output"];
+  timestamp: Scalars["Timestamp"]["output"];
+  type: Scalars["String"]["output"];
 };
 
 export type LoginInput = {
-  email: Scalars['String']['input'];
-  password: Scalars['String']['input'];
+  email: Scalars["String"]["input"];
+  password: Scalars["String"]["input"];
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
-  createDocument: Scalars['Boolean']['output'];
+  __typename?: "Mutation";
+  createDocument: Scalars["Boolean"]["output"];
   createUser: UserDto;
-  deleteFileInFolder: Scalars['Boolean']['output'];
+  deleteFileInFolder: Scalars["Boolean"]["output"];
   login: AuthResponse;
   logs: Array<Log>;
-  updateDocument: Scalars['Boolean']['output'];
-  updateDocumentContent: Scalars['Boolean']['output'];
+  updateDocument: Scalars["Boolean"]["output"];
+  updateDocumentContent: Scalars["Boolean"]["output"];
 };
-
 
 export type MutationCreateDocumentArgs = {
-  file: Scalars['Upload']['input'];
+  file: Scalars["Upload"]["input"];
 };
-
 
 export type MutationCreateUserArgs = {
   input: CreateUserInput;
 };
 
-
 export type MutationDeleteFileInFolderArgs = {
   input: DeleteFileInput;
 };
-
 
 export type MutationLoginArgs = {
   input: LoginInput;
 };
 
-
 export type MutationLogsArgs = {
   input: GetLogsInput;
 };
 
-
 export type MutationUpdateDocumentArgs = {
   input: UpdateDocumentInput;
 };
-
 
 export type MutationUpdateDocumentContentArgs = {
   input: UpdateDocumentContentInput;
 };
 
 export type Query = {
-  __typename?: 'Query';
+  __typename?: "Query";
   getCurrentUser: Maybe<UserDto>;
-  getDocumentContent: Scalars['String']['output'];
-  getFileUrl: Scalars['String']['output'];
+  getDocumentContent: Scalars["String"]["output"];
+  getFileUrl: Scalars["String"]["output"];
   getUserById: Maybe<UserDto>;
   getUsers: Array<UserDto>;
-  hello: Scalars['String']['output'];
+  hello: Scalars["String"]["output"];
   listFilesInFolder: Array<Document>;
 };
 
-
 export type QueryGetDocumentContentArgs = {
-  id: Scalars['Float']['input'];
+  id: Scalars["Float"]["input"];
 };
 
-
 export type QueryGetFileUrlArgs = {
-  id: Scalars['Float']['input'];
+  id: Scalars["Float"]["input"];
 };
 
 /** User role (admin or user) */
-export enum Role {
-  Admin = 'ADMIN',
-  User = 'USER'
-}
+export type Role = "ADMIN" | "USER";
 
 export type UpdateDocumentContentInput = {
-  content: Scalars['String']['input'];
-  filename: Scalars['String']['input'];
-  id: Scalars['Int']['input'];
+  content: Scalars["String"]["input"];
+  filename: Scalars["String"]["input"];
+  id: Scalars["Int"]["input"];
 };
 
 export type UpdateDocumentInput = {
-  file: Scalars['Upload']['input'];
-  id: Scalars['Int']['input'];
+  file: Scalars["Upload"]["input"];
+  id: Scalars["Int"]["input"];
 };
 
 export type User = {
-  __typename?: 'User';
+  __typename?: "User";
   documents: Maybe<Array<Document>>;
-  email: Scalars['String']['output'];
-  id: Scalars['Int']['output'];
-  name: Maybe<Scalars['String']['output']>;
+  email: Scalars["String"]["output"];
+  id: Scalars["Int"]["output"];
+  name: Maybe<Scalars["String"]["output"]>;
   role: Role;
 };
 
 export type UserDto = {
-  __typename?: 'UserDTO';
-  email: Scalars['String']['output'];
-  id: Scalars['Int']['output'];
-  name: Maybe<Scalars['String']['output']>;
+  __typename?: "UserDTO";
+  email: Scalars["String"]["output"];
+  id: Scalars["Int"]["output"];
+  name: Maybe<Scalars["String"]["output"]>;
   role: Role;
 };
